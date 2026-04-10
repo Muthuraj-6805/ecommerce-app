6ebda357423d1504d0d02a7062afab203b229d55
Muthuraj <muthuraj68m@gmail.com>
2026-04-10 17:06:11 +0530

    Initial commit of e-commerce PWA

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    NgFor,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('myshop-app');
+
+  products = [
+  {
+    name: 'Wireless Headphones',
+    price: 99,
+    image: 'headphones.jpg'
+  },
+  {
+    name: 'Smart Watch',
+    price: 149,
+    image: 'watch.jpg'
+  },
+  {
+    name: 'Gaming Mouse',
+    price: 59,
+    image: 'mouse.jpg'
+  }
+  ];
+
 }
